9f0d146e286b7e579117ffb893dacd17c3205d1a
Jorge R <jorgerodriguez434@gmail.com>
2018-09-12 08:36:30 -0500

    add heroku url

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://client-hour-tracker.herokuapp.com';
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "https://client-hour-tracker.herokuapp.com";
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL ||
                  'mongodb://localhost/client-hour-tracker';
 exports.PORT = process.env.PORT || 8080;
+
+//https://client-hour-tracker.herokuapp.com
